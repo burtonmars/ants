a6ea17c47c35d3c6ed56737895be55951987cede
Marshal_desktop <burtonmars91@gmail.com>
2019-11-04 20:58:58 -0800

    ants bouncing around the room in different colors

---
+++ ant.js
@@ -0,0 +1,41 @@
+
+var minAntSpeed = 260;
+let maxAntSpeed = 290;
+
+class Ant {
+
+    constructor(color) {
+        this.x = windowWidth / 2;
+        this.y = windowHeight / 2;
+        this.xspeed = Math.random(minAntSpeed, maxAntSpeed) * (Math.round(Math.random()) * 2 -1);
+        this.yspeed = Math.random(minAntSpeed, maxAntSpeed) * (Math.round(Math.random()) * 2 -1);
+        this.color = color;
+        this.size = 12;
+    }
+
+    update() {
+        if (this.x >= (width - this.size / 2) || this.x <= 0) {
+            this.xspeed = -this.xspeed;
+        }
+        if (this.y >= (height - this.size / 2) || this.y <= 0) {
+            this.yspeed = -this.yspeed;
+        }
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+
+    show() {
+        switch(this.color) {
+            case "green":
+                fill(color(0, 255, 0));
+                break;
+            case "blue":
+                fill(color(0, 0, 255));
+                break;
+            case "red":
+                fill(color(255, 0, 0));
+                break;
+        }
+        ellipse(this.x, this.y, this.size);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+const windowHeight = 600;
+const windowWidth = 600;
+const speed = 4;
+
+let greAnt;
+let redAnt;
+let bluAnt;
+
+let redAnts = [];
+let bluAnts = [];
+let greAnts = [];
+let allAnts = [];
+
+let numGreAnts = 33;
+let numRedAnts = 33;
+let numBluAnts = 33;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	for (let i = 0; i < numGreAnts; i++) {
+		greAnt = new Ant("green");
+		greAnts.push(greAnt);
+	}
+	for (let i = 0; i < numRedAnts; i++) {
+		redAnt = new Ant("red");
+		redAnts.push(redAnt);
+	}
+	for (let i = 0; i < numBluAnts; i++) {
+		bluAnt = new Ant("blue");
+		bluAnts.push(bluAnt);
+	}
+	allAnts = (greAnts.concat(redAnts)).concat(bluAnts);
+
 }
 
 function draw() {
-
+	background(20);
+	for (const ant of allAnts) {
+		ant.update();
+		ant.show();
+	}
 }
