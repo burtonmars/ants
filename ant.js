6e639c82a53726fcd7e56e76926d58cbe44b0d73
Marshal_desktop <burtonmars91@gmail.com>
2019-11-05 17:42:45 -0800

    added todo's in ant.js for algorithms about changing color

--- ant.js
+++ ant.js
@@ -1,23 +1,26 @@
 const minAntSpeed = 260;
 const maxAntSpeed = 290;
-const ferimoneBuffer = 20;
 
 class Ant {
 
-    constructor(color) {
+    constructor(color, buff) {
         this.x = windowWidth / 2;
         this.y = windowHeight / 2;
         this.xspeed = Math.random(minAntSpeed, maxAntSpeed) * (Math.round(Math.random()) * 2 -1);
         this.yspeed = Math.random(minAntSpeed, maxAntSpeed) * (Math.round(Math.random()) * 2 -1);
         this.color = color;
         this.size = 12;
+        this.ferimoneBuffer = buff; // measured by the radius in pixels
+        this.redAntCount = 0;
+        this.blueAntCount = 0;
+        this.greenAntCount = 0;
     }
 
     update() {
-        if (this.x >= (width - this.size / 2) || this.x <= 0) {
+        if (this.x >= (width - this.size / 2) || this.x <= this.size / 2) {
             this.xspeed = -this.xspeed;
         }
-        if (this.y >= (height - this.size / 2) || this.y <= 0) {
+        if (this.y >= (height - this.size / 2) || this.y <= this.size / 2) {
             this.yspeed = -this.yspeed;
         }
         this.x += this.xspeed;
@@ -39,7 +42,23 @@ class Ant {
         ellipse(this.x, this.y, this.size, this.size-this.size / 1.8);
     }
 
-    changeJob() {
+    // checks current counts of all colors of ants seen and decides whether 
+    // to change color and returns a color (same color if it decides not
+    // to change)
+    checkChangeJob() {
+        // TODO: compare current this.antcount's, if current color has highest seen,
+        // change to lowest, otherwise stay the same 
+        return "green"; //STUB
+    }
 
+    // checks list of other ants to see if it overlaps any of their buffers
+    // - if it does, check color and add some ammount to this.*antcount for that
+    //   color (amt added should be proportional to both ants speed and the size
+    //   of the buffer so that the length of the encounter * amt added equals one)
+    countNearbyAnts(ants) {
+        for (const a of ants) {
+            // TODO: check if in buffer of ant a, check ant a's color and speed,
+            // add appropriate amt to this.*antcount 
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 const windowHeight = 400;
 const windowWidth = 700;
 const speed = 4;
-const totalAnts = 120;
 
+let totalAnts;
 let greAnt, redAnt, bluAnt;
 
 let redAnts = [];
@@ -10,9 +10,12 @@ let bluAnts = [];
 let greAnts = [];
 let allAnts = [];
 
+totalAnts = 180;
 let numGreAnts = totalAnts/3;
 let numRedAnts = totalAnts/3;
 let numBluAnts = totalAnts/3;
+
+let buffer;
 
 function setup() {
 	let myCanvas = createCanvas(windowWidth, windowHeight);
@@ -20,20 +23,20 @@ function setup() {
 	let input1 = createInput();
 	let input2 = createInput();
 	let input3 = createInput();
-	input1.parent();
-	input2.parent();
-	input3.parent();
+	input1.parent("input1");
+	input2.parent("input2");
+	input3.parent("input3");
 	// interval = setInterval(updateNumberAnts, 5000);
 	for (let i = 0; i < numGreAnts; i++) {
-		greAnt = new Ant("green");
+		greAnt = new Ant("green", buffer);
 		greAnts.push(greAnt);
 	}
 	for (let i = 0; i < numRedAnts; i++) {
-		redAnt = new Ant("red");
+		redAnt = new Ant("red", buffer);
 		redAnts.push(redAnt);
 	}
 	for (let i = 0; i < numBluAnts; i++) {
-		bluAnt = new Ant("blue");
+		bluAnt = new Ant("blue", buffer);
 		bluAnts.push(bluAnt);
 	}
 	allAnts = (greAnts.concat(redAnts)).concat(bluAnts);
@@ -41,12 +44,16 @@ function setup() {
 	let simulate = createButton("simulate");
 	simulate.parent('simul');
 	simulate.mousePressed(draw);
+
 }
 
 function draw() {
 	background(51);
 	for (const ant of allAnts) {
+		let otherAnts = allAnts.slice();
+		otherAnts.splice(allAnts[allAnts.indexOf(ant)], 1);
 		ant.update();
 		ant.show();
+		ant.countNearbyAnts(otherAnts);
 	}
 }
